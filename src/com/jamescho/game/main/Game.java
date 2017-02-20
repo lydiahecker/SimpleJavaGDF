01469c5b0ad8105ed9131d4a53c9480ddb1cd198
Chris Ortman <chris-ortman@uiowa.edu>
2017-02-19 22:22:46 -0600

    Add game loop

--- src/com/jamescho/game/main/Game.java
+++ src/com/jamescho/game/main/Game.java
@@ -10,7 +10,7 @@ import java.awt.*;
  * Created by cortman on 2/14/17.
  */
 @SuppressWarnings("serial")
-public class Game extends JPanel {
+public class Game extends JPanel implements Runnable {
     private int gameWidth;
     private int gameHeight;
     private Image gameImage;
@@ -38,5 +38,53 @@ public class Game extends JPanel {
     public void addNotify() {
         super.addNotify();
         setCurrentState(new LoadState());
+        initGame();
+    }
+
+    public void exit() {
+        running = false;
+    }
+
+    private void initGame() {
+        running = true;
+        gameThread = new Thread(this, "Game Thread");
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        while(running) {
+            System.out.println("Run GameLoop");
+            
+            currentState.update();
+            prepareGameImage();
+            currentState.render(gameImage.getGraphics());
+            repaint();
+            try {
+                Thread.sleep(14);
+            } catch(InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.exit(0);
+    }
+
+    private void prepareGameImage() {
+        if( gameImage == null ) {
+            gameImage = createImage(gameWidth, gameHeight);
+        }
+
+        Graphics g = gameImage.getGraphics();
+        g.clearRect(0,0,gameWidth, gameHeight);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if ( gameImage == null ) {
+            return;
+        }
+
+        g.drawImage(gameImage, 0, 0, null);
     }
 }
